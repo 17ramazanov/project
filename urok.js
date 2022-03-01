1a3a0661c1d868347a2d90145a0477460d3d6e37
Ramazanov <17ramazanov@mail.ru>
2022-03-01 22:38:35 +0300

    first commit

---
+++ urok.js
@@ -0,0 +1,35 @@
+//72Ramazanov
+"use strict";
+
+// let a = document.querySelector('h1');
+
+
+// a.classList.add('blue', 'font')
+// let a = document.querySelector('body');
+// var n = prompt("Хотите сменить тему на черный").toLowerCase;
+
+// if (n === 'да' || 'yes'){
+//     console.log(a.classList.add('red'))
+// } else {
+    
+// }
+
+// let img = document.querySelector('img')
+
+// function rnr(){
+//     console.log(img.getAttribute('src'))
+//     img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLyY-2yaYYEESJi82asMlVsk43nywVXjuTLw&usqp=CAU')
+//     console.log(img.getAttribute('src'));
+// }
+
+// setTimeout(rnr,2000)
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// console.log(document.body.hasChildNodes());
+
+// for(let i of document.body.childNodes) {
+//     console.log(i)
+// }
